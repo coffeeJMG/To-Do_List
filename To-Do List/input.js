238be8ca247b23c6aaf27c05787ff85f2ac8a8de
coffeeJMG <114897197+coffeeJMG@users.noreply.github.com>
2023-03-30 15:51:34 +0900

    delete btn
    이벤트 리스너 함수안에 모두 적던걸
    콜백함수 이용해서 정리하고
    
    삭제버튼을 추가해서 리스트 삭제
    기능추가

--- To-Do List/input.js
+++ To-Do List/input.js
@@ -1,56 +1,76 @@
 let textCount=0;
 let boxCount=0;
 
+document.querySelector('.toDoList_contents_btn').addEventListener('click',addList)
+
+    
+
+function addList(){
 
 
-document.querySelector('.toDoList_contents_btn').addEventListener('click',function(){
-
-   
     const newText_input = document.querySelector('.toDoList_contents_text').value;
     
-   
-    if(newText_input ==''){
+   if(newText_input ==''){
         alert("넌 목표도없냐 이새끼야")
     }else{
         
+        const new_contents = document.createElement('div')
+        new_contents.className='new_toDoList_contents';
+
         const newText = 
         `
         <input type="text" value=" ${newText_input}"  class="new_contents_text" >
-        <input type="checkbox" class="content_checkbox" ><lable for="checkbox"></lable>
-        <input type="submit" class="contents_deleteBtn" value="삭제">
-        </div>
+        <input type="checkbox" class="content_checkbox" >
         `;
+
         const newList = document.createElement('div');
         newList.className='new_toDoList_contents';
         newList.innerHTML = newText;
+
         
+        const newBtn =document.createElement('button');
+        newBtn.className="contents_deleteBtn";
+        newBtn.innerHTML='삭제';
         
-        const newCheckbox = newList.querySelector('.content_checkbox');
-        newCheckbox.addEventListener('click', function() {
+        newBtn.addEventListener('click',delList)
+        
+        new_contents.appendChild(newList);
+        new_contents.appendChild(newBtn);
 
-            
-            boxCount++;
-            if(boxCount ===textCount && boxCount>0){
+        document.querySelector('.main_column_toDoList').appendChild(new_contents)
 
-                document.getElementsByClassName('screen-header__message')[0].innerHTML="힘내라 내일보자"
-            }
-            const checkList = this.parentNode.querySelector('.new_contents_text');
-            if(checkList.style.textDecoration === 'line-through'){
-                checkList.style.textDecoration = 'none'
-            }else{
-                checkList.style.textDecoration = 'line-through'
-            }
-          
-        });
 
-        document.querySelector('.main_column_toDoList').appendChild(newList);
         textCount++;
 
-         }
-         
-      
+       
 
-   
+        const newCheckbox = newList.querySelector('.content_checkbox');
+        newCheckbox.addEventListener('click', checkBox );
+    
+            }
+         
+        }
+
+    function delList(event){
+
+         const target = event.target.parentElement;
+         target.remove();
+         console.log(target)
+     }
+
+     function checkBox(){
+
+         boxCount++;
+         if(boxCount ===textCount && boxCount>0){
+
+            document.getElementsByClassName('screen-header__message')[0].innerHTML="힘내라 내일보자"
+         }
+         const checkList = this.parentNode.querySelector('.new_contents_text');
+         if(checkList.style.textDecoration === 'line-through'){
+                checkList.style.textDecoration = 'none'
+         }else{
+                checkList.style.textDecoration = 'line-through'
+         }
         
-})
+     }
 
